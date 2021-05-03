af51231c28ce2c40babdb8187b0d0922d67235af
sergio.tellez <ST&109tel>
2021-05-03 18:07:41 -0500

    Deploying to heroku

--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@ import app from './src/app'
 import './src/database'
 import express from "express";
 
+const path = require('path');
+
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'))
 })
 
 app.use(express.static('public'))
 app.use(express.static('files'))
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
