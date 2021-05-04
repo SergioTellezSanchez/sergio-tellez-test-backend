2847dee6d95a6acefd408d58fece25257341a441
sergio.tellez <ST&109tel>
2021-05-03 19:33:47 -0500

    Fixing database hosting

--- index.ts
+++ index.ts
@@ -1,17 +1,6 @@
 import app from './src/app'
 import './src/database'
-import express from "express";
-
-const path = require('path');
-
-app.use(express.static('public'))
-app.use(express.static('files'))
-
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-})
 
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'))
 })
-;
